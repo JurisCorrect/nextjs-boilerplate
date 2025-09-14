b2311d1c1d4abed66f7b725b6a83efea2c0372f3
JurisCorrect <marie.terki@icloud.com>
2025-09-14 21:36:48 +0200

    Update page.js

--- app/merci/page.js
+++ app/merci/page.js
@@ -4,11 +4,9 @@ import { redirect } from 'next/navigation'
 export default function Merci({ searchParams }) {
   const qs = new URLSearchParams()
   const sp = searchParams ?? {}
-
   for (const [k, v] of Object.entries(sp)) {
     if (Array.isArray(v)) v.forEach(val => qs.append(k, String(val)))
     else if (v != null) qs.set(k, String(v))
   }
-
   redirect(`/merci2${qs.toString() ? `?${qs.toString()}` : ''}`)
 }
