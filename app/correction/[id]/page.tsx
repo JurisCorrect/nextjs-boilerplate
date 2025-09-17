b3b4f668fa39c38bb23187760ad275d720d7d287
JurisCorrect <marie.terki@icloud.com>
2025-09-17 19:14:28 +0200

    Update page.tsx

--- app/correction/[id]/page.tsx
+++ app/correction/[id]/page.tsx
@@ -1,16 +1,13 @@
 // app/correction/[id]/page.tsx
-import TeaserClient from "../TeaserClient"
+import AnnotatedTeaser from "../AnnotatedTeaser"
 
 export const dynamic = "force-dynamic"
 
-type Props = { params: { id: string } }
-
-export default async function CorrectionPage({ params }: Props) {
-  const id = params.id
+export default function CorrectionPage({ params }: { params: { id: string } }) {
   return (
     <main className="page-wrap correction">
       <h1 className="page-title">CORRECTION</h1>
-      <TeaserClient submissionId={id} />
+      <AnnotatedTeaser submissionId={params.id} />
     </main>
   )
 }
